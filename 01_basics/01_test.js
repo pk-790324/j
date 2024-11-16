d8ada2ba282018c498dfbb73dded2edd5e9039d3
pk-7342 <pappuyadav98199@gmail.com>
2024-11-16 20:36:13 +0545

    first commit

---
+++ 01_basics/01_test.js
@@ -0,0 +1,54 @@
+//console.log("welcome to the world of node.js")
+
+
+// let movies = null;
+// console.log(typeof(movies))
+//output:object
+
+
+// let movies;
+// console.log(typeof(movies))
+// output:undefined
+
+// let st ="this is"
+// console.log(typeof(st))
+// output:string
+
+
+// let st="234asd"
+// let ti = Number(st)
+// console.log(typeof(ti))
+// console.log(ti)
+// output:number
+// output:NaN
+// Description:here st="234asd" is converted to number as shown in output but output is not a number(NaN)
+
+
+// let st=null
+// let ti = Number(st)
+// console.log(typeof(ti))
+// console.log(ti)
+// output:number
+// output:0
+
+// let st=undefined
+// let ti = Number(st)
+// console.log(typeof(ti))
+// console.log(ti)
+// output:number
+// output:NaN
+
+// let name ="pappu"
+// let conv =Boolean(name)
+// console.log(conv)
+// console.log(typeof(conv))
+//output:true
+//output:boolean
+
+
+//let num =333
+//let st =String(num)
+//console.log(st)
+//console.log(typeof(st))
+//output:333
+//output:string
